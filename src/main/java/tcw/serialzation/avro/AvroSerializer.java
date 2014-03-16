f2fb68904e31a77ee5cb45039bbb42493b02b167
tcw <tcwgit@gmail.com>
2014-03-16 21:25:09 +0100

    Added avro (improvment)

--- src/main/java/tcw/serialzation/avro/AvroSerializer.java
+++ src/main/java/tcw/serialzation/avro/AvroSerializer.java
@@ -1,5 +1,6 @@
 package tcw.serialzation.avro;
 
+import com.google.common.collect.Lists;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.file.SeekableByteArrayInput;
@@ -15,6 +16,8 @@ import tcw.domain.v1.EmployeeV1;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AvroSerializer {
@@ -23,18 +26,25 @@ public class AvroSerializer {
     DataFileWriter<EmployeeAvroV1> dataFileWriter = new DataFileWriter<EmployeeAvroV1>(userDatumWriter);
     DatumReader<EmployeeAvroV1> userDatumReader = new SpecificDatumReader<EmployeeAvroV1>(EmployeeAvroV1.class);
 
-    public byte[] serialize(EmployeeAvroV1 employeeV1) throws IOException {
+    public byte[] serialize(List<EmployeeAvroV1> employeeV1) throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        dataFileWriter.create(employeeV1.getSchema(), bos);
-        dataFileWriter.append(employeeV1);
+        dataFileWriter.create(employeeV1.get(0).getSchema(), bos);
+        for (EmployeeAvroV1 employeeAvroV1 : employeeV1) {
+            dataFileWriter.append(employeeAvroV1);
+        }
         dataFileWriter.close();
         return bos.toByteArray();
     }
 
-    public EmployeeAvroV1 deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+    public List<EmployeeAvroV1> deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
         SeekableByteArrayInput sbai = new SeekableByteArrayInput(bytes);
         DataFileReader<EmployeeAvroV1> dataFileReader = new DataFileReader<EmployeeAvroV1>(sbai, userDatumReader);
+        EmployeeAvroV1 employeeAvroV1 = null;
+        ArrayList<EmployeeAvroV1> employees = Lists.newArrayList();
+        while (dataFileReader.hasNext()) {
+            employees.add(dataFileReader.next(employeeAvroV1));
+        }
         sbai.close();
-        return dataFileReader.next();
+        return employees;
     }
 }
--- src/test/java/tcw/serialzation/avro/AvroSerializerTest.java
+++ src/test/java/tcw/serialzation/avro/AvroSerializerTest.java
@@ -1,9 +1,12 @@
 package tcw.serialzation.avro;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import tcw.domain.avro.java.EmployeeAvroV1;
 import tcw.domain.util.Populated;
+
+import java.util.List;
 
 public class AvroSerializerTest {
 
@@ -11,21 +14,22 @@ public class AvroSerializerTest {
     public void validation() throws Exception {
         AvroSerializer avroSerializer = new AvroSerializer();
         EmployeeAvroV1 employeeV1 = Populated.avroEmployee();
-        byte[] serializedEmployee = avroSerializer.serialize(employeeV1);
-        EmployeeAvroV1 deserializedEmployeeV1 = avroSerializer.deserialize(serializedEmployee);
-        System.out.println(deserializedEmployeeV1);
+        byte[] serializedEmployee = avroSerializer.serialize(Lists.newArrayList(employeeV1));
+        List<EmployeeAvroV1> employeeAvroV1s = avroSerializer.deserialize(serializedEmployee);
+        System.out.println(employeeAvroV1s.get(0));
     }
 
     @Test
     public void benchmark() throws Exception {
         AvroSerializer avroSerializer = new AvroSerializer();
-        EmployeeAvroV1 employeeV1 = Populated.avroEmployee();
         EmployeeAvroV1 deserializedEmployeeV1 = null;
-        Stopwatch stopwatch = Stopwatch.createStarted();
+        List<EmployeeAvroV1> avorEmployees = Lists.newArrayList();
         for (int i = 0; i < 1000000; i++) {
-            byte[] serializedEmployee = avroSerializer.serialize(employeeV1);
-            deserializedEmployeeV1 = avroSerializer.deserialize(serializedEmployee);
+            avorEmployees.add(Populated.avroEmployee());
         }
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        byte[] serializedEmployee = avroSerializer.serialize(avorEmployees);
+        List<EmployeeAvroV1> deserialized = avroSerializer.deserialize(serializedEmployee);
         stopwatch.stop();
         System.out.println(stopwatch.toString());
     }
